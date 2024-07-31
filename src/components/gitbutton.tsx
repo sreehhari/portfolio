662839c031439e7064fb8004b59cbc7d50f0b3f1
Sreehari Sujith <sreehari9300@gmail.com>
2024-07-31 22:32:31 +0530

    added github button

--- src/components/gitbutton.tsx
+++ src/components/gitbutton.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { HoverBorderGradient } from "./ui/hover-border-gradient";
+import { MarkGithubIcon } from "@primer/octicons-react";
 export default function gitbutton() {
   return (
     <div className="fixed top-14 right-10  z-50">
@@ -10,6 +11,7 @@ export default function gitbutton() {
         className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
       >
         {/* <AceternityLogo /> */}
+        <MarkGithubIcon size={16} />
         <span>GitHub</span>
       </HoverBorderGradient>
     </div>
